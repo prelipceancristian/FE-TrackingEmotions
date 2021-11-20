eac0c407d7799687a1c974e117d5408942fca8cd
Emilia <emiliasml@yahoo.com>
2021-11-20 13:22:36 +0200

    deleted an unused import

--- tracking_emotions/lib/widgets/after-submit/congratiulations-page.dart
+++ tracking_emotions/lib/widgets/after-submit/congratiulations-page.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:tracking_emotions/widgets/dialogs/pop-up-question.dart';
 
 class CongratulationsPage extends StatelessWidget {
   @override
