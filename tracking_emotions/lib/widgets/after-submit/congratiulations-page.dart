e439aefb1ed85abf6b60cd7dff58d9a9de25baeb
Zubii <ionutzubascu@yahoo.com>
2022-01-12 15:17:34 +0200

    fix navigation problem

--- tracking_emotions/lib/widgets/after-submit/congratiulations-page.dart
+++ tracking_emotions/lib/widgets/after-submit/congratiulations-page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:tracking_emotions/widgets/emotions_report_page/emotions_report_page.dart';
 
 class CongratulationsPage extends StatelessWidget {
   @override
@@ -11,10 +12,7 @@ class CongratulationsPage extends StatelessWidget {
               margin: EdgeInsets.only(top: 350),
               child: Text(
                 'Congratulations! \n You have logged your first emotion!',
-                style: TextStyle(
-                    fontSize: 35,
-                    fontWeight: FontWeight.bold,
-                    color: Colors.brown[700]),
+                style: TextStyle(fontSize: 35, fontWeight: FontWeight.bold, color: Colors.brown[700]),
                 textAlign: TextAlign.center,
               ),
             ),
@@ -22,6 +20,10 @@ class CongratulationsPage extends StatelessWidget {
         ),
       ),
       backgroundColor: Colors.red[100],
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.arrow_forward_ios),
+        onPressed: () => Navigator.pushReplacement(context, MaterialPageRoute(builder: (context) => EmotionsReportPage())),
+      ),
     );
   }
 }
