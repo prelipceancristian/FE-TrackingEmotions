5fe8b204e8621b3b6274c9d1c4073d02478cc5fc
Prelipcean Cristian <prelipceancristian100@gmail.com>
2022-01-15 20:33:24 +0200

    Info page emotion retrieve

--- tracking_emotions/lib/widgets/emotions_info_page/emotion_info_page.dart
+++ tracking_emotions/lib/widgets/emotions_info_page/emotion_info_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:tracking_emotions/models/emotion.dart';
+import 'package:tracking_emotions/utils/services/Emotion-service.dart';
 
 class EmotionInfoPage extends StatelessWidget {
   const EmotionInfoPage({key}) : super(key: key);
@@ -22,74 +24,42 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  // This holds a list of fiction users
-  // You can use data fetched from a database or a server as well
-  final List<Map<String, dynamic>> _allUsers = [
-    {
-      "id": 1,
-      "name": "Fericire",
-      "description":
-          "Fericirea este o stare mentală de bine, fiind caracterizată de emoţii pozitive sau plăcute, de la mulţumire la bucurie intensă.",
-    },
-    {
-      "id": 2,
-      "name": "Ușurare",
-      "description": "Te simți eliberat de sub apăsarea unui lucru greu.",
-    },
-    {
-      "id": 3,
-      "name": "Mulțumire",
-      "description":
-          "Sfaturi: Cultiva-ti vorbirea intr-o voce calda, joasa. Nu lasa niciodata sa treaca o ocazie fara sa incurajezi pe cineva sau sa spui ceva frumos cuiva.",
-    },
-    {
-      "id": 4,
-      "name": "Binecuvântare",
-      "description":
-          "Fă o listă de lucruri și oameni pentru care ești recunoscător în viața ta.",
-    },
-    {
-      "id": 5,
-      "name": "Încântare",
-      "description": "Ce ai auzit/văzut/simțit/experimentat astăzi frumos?",
-    },
-    {
-      "id": 6,
-      "name": "Amuzare",
-      "description": "Ai auzit o glumă bună?",
-    },
-    {
-      "id": 7,
-      "name": "Mândrie",
-      "description": "Ce ai realizat astăzi minunat?",
-    },
-    {
-      "id": 8,
-      "name": "Răsplătire",
-      "description":
-          "Ai muncit din greu pentru ceva anume și ai fost remunerat pe măsură.",
-    },
-    {
-      "id": 9,
-      "name": "Satisfăcere",
-      "description":
-          "Ai muncit din greu pentru ceva anume și ți-a ieșit cum ți-ai propus.",
-    },
-    {
-      "id": 10,
-      "name": "Optimism",
-      "description":
-          "Orice s-ar întâmpla acum, vezi partea pozitivă a lucrurilor.",
-    },
-  ];
-
-  // This list holds the data for the list view
+  List<Map<String, dynamic>> _allUsers = [];
   List<Map<String, dynamic>> _foundUsers = [];
+  EmotionService _emotionService = EmotionService();
+  var _controller = TextEditingController();
+
+  Future<List<Map<String, dynamic>>> getEmotions() async {
+    List<Map<String, dynamic>> menuItems = [];
+    List<Emotion> emotionList =
+        await this._emotionService.getEmotionsForCategory("-1");
+
+    emotionList.forEach((emotion) {
+      menuItems.add({
+        "id": emotion.EmotionId,
+        "name": emotion.Name,
+        "description": emotion.DescriptionText,
+        "descriptionId": emotion.DescriptionId
+        //TO DO: must bring the text Description here!!
+      });
+    });
+    return menuItems;
+  }
+
   @override
   initState() {
     // at the beginning, all users are shown
-    _foundUsers = _allUsers;
+    setMenuItems();
     super.initState();
+  }
+
+  setMenuItems() async {
+    var backendMenuItems = await getEmotions();
+    print(backendMenuItems);
+    this.setState(() {
+      _allUsers = backendMenuItems;
+      _foundUsers = _allUsers;
+    });
   }
 
   // This function is called whenever the text field changes
@@ -105,70 +75,97 @@ class _HomePageState extends State<HomePage> {
           .toList();
       // we use the toLowerCase() method to make it case-insensitive
     }
-
-    // Refresh the UI
     setState(() {
       _foundUsers = results;
     });
+  }
+
+  void clear() {
+    _controller.clear();
+    _runFilter(_controller.text);
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Color.fromARGB(255, 250, 237, 227),
-      body: Padding(
-        padding: const EdgeInsets.all(10),
-        child: Column(
-          children: [
-            const SizedBox(
-              height: 20,
-            ),
-            TextField(
-                onChanged: (value) => _runFilter(value),
-                decoration: InputDecoration(
-                    prefixIcon: Icon(Icons.search),
-                    labelText: 'Search for an emotion',
-                    border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(30.0)),
-                    filled: true,
-                    fillColor: Colors.white70)),
-            const SizedBox(
-              height: 20,
-            ),
-            Expanded(
-              child: _foundUsers.isNotEmpty
-                  ? ListView.builder(
-                      itemCount: _foundUsers.length,
-                      itemBuilder: (context, index) => Card(
-                        shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(30.0)),
-                        key: ValueKey(_foundUsers[index]["id"]),
-                        color: Color.fromARGB(255, 249, 187, 178),
-                        elevation: 10,
-                        margin: const EdgeInsets.symmetric(
-                            vertical: 14, horizontal: 10),
-                        child: ListTile(
-                          title: Padding(
-                              padding:
-                                  EdgeInsets.only(left: 12, top: 8, right: 12),
-                              child: Text(_foundUsers[index]['name'],
-                                  style: const TextStyle(fontSize: 24))),
-                          subtitle: Padding(
-                              padding: EdgeInsets.only(
-                                  left: 9, top: 8, right: 9, bottom: 25),
-                              child: Text(_foundUsers[index]['description'],
-                                  style: const TextStyle(fontSize: 18))),
-                        ),
-                      ),
+      body: FutureBuilder(
+          future: _emotionService.getEmotionsForCategory("-1"),
+          builder: (context, snapshot) {
+            return Padding(
+              padding: const EdgeInsets.all(10),
+              child: snapshot.connectionState == ConnectionState.waiting
+                  ? Center(
+                      child: CircularProgressIndicator(),
                     )
-                  : const Text(
-                      'No results found',
-                      style: TextStyle(fontSize: 24),
+                  : Column(
+                      children: [
+                        const SizedBox(
+                          height: 20,
+                        ),
+                        TextField(
+                          //onChanged: (value) => _runFilter(value),
+                          controller: _controller,
+                          decoration: InputDecoration(
+                              suffixIcon: IconButton(
+                                onPressed: () => _runFilter(_controller.text),
+                                icon: Icon(Icons.search),
+                              ),
+                              prefixIcon: IconButton(
+                                  onPressed: this.clear,
+                                  icon: Icon(Icons.clear)),
+                              labelText: 'Search for an emotion',
+                              border: OutlineInputBorder(
+                                  borderRadius: BorderRadius.circular(30.0)),
+                              filled: true,
+                              fillColor: Colors.white70),
+                        ),
+                        const SizedBox(
+                          height: 20,
+                        ),
+                        Expanded(
+                          child: _allUsers.isNotEmpty
+                              ? ListView.builder(
+                                  itemCount: _foundUsers.length,
+                                  itemBuilder: (context, index) => Card(
+                                    shape: RoundedRectangleBorder(
+                                        borderRadius:
+                                            BorderRadius.circular(30.0)),
+                                    key: ValueKey(_foundUsers[index]["id"]),
+                                    color: Color.fromARGB(255, 249, 187, 178),
+                                    elevation: 10,
+                                    margin: const EdgeInsets.symmetric(
+                                        vertical: 14, horizontal: 10),
+                                    child: ListTile(
+                                      title: Padding(
+                                          padding: EdgeInsets.only(
+                                              left: 12, top: 8, right: 12),
+                                          child: Text(
+                                              _foundUsers[index]['name'],
+                                              style: const TextStyle(
+                                                  fontSize: 24))),
+                                      subtitle: Padding(
+                                          padding: EdgeInsets.only(
+                                              left: 9,
+                                              top: 8,
+                                              right: 9,
+                                              bottom: 25),
+                                          child: Text(
+                                              _foundUsers[index]['description'],
+                                              style: const TextStyle(
+                                                  fontSize: 18))),
+                                    ),
+                                  ),
+                                )
+                              : const Text(
+                                  'No results found',
+                                  style: TextStyle(fontSize: 24),
+                                ),
+                        ),
+                      ],
                     ),
-            ),
-          ],
-        ),
-      ),
+            );
+          }),
     );
   }
 }
